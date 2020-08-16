c2879fe51a39450edfbd214ed69520b811fc3dc3
opykhtin-sv <seropyh@yandex.ru>
2020-08-16 12:24:13 +0300

    two homeworks

---
+++ HomeWork1/src/com/homework1/OneActionWithNumber.java
@@ -0,0 +1,19 @@
+package com.homework1;
+
+import java.util.Scanner;
+
+public class OneActionWithNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		var scn = new Scanner(System.in);
+
+		var a = scn.nextDouble();
+		var b = scn.nextDouble();
+		var c = a - b;
+		System.out.println(c);
+
+	}
+
+}
