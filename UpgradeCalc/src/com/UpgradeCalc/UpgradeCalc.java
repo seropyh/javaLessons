432e7c2a5234938a3ee4f80d2c7252f019bb84fa
sergei <seropyh@yandex.ru>
2020-10-24 01:32:45 +0300

    Change calculator

--- UpgradeCalc/src/com/UpgradeCalc/UpgradeCalc.java
+++ UpgradeCalc/src/com/UpgradeCalc/UpgradeCalc.java
@@ -1,75 +1,82 @@
-package com.UpgradeCalc;
+package com.upgradecalc;
 
 import java.util.Scanner;
 
 public class UpgradeCalc {
 
-	public static double myPow(double n1, long n2) {
-		double result = n1;
-		if (n2 == 0)
-			return 1;
-		result = n1;
-		for (int i = 1; i < n2; i++) {
-			result = result * n1;
-		}
-		return result;
-	}
+    public static int myPow(int n1, int n2) {
+        int result = n1;
+        if (n2 == 0) {
+            return 1;
+        }
+        for (int i = 1; i < n2; i++) {
+            result = result * n1;
+        }
+        return result;
 
-	public static void main(String[] args) {
-		var sc = new Scanner(System.in);
-		var action = "";
-		var number1 = 0d;
-		var number2 = 0d;
-		var result = 0d;
-		var goodAction = true;
-		System.out.println("Введите действие(/,+,-,*,^(второе чило целое),mod,div):");
-		do {
-			action = sc.next();
-			goodAction = action.equals("/") || action.equals("+") || action.equals("-") || action.equals("*")
-					|| action.equals("^") || action.equals("mod") || action.equals("div");
+    }
 
-			if (!goodAction) {
-				System.out.println("Неверное действие.\nВведите действие(/,+,-,*,^(второе число целое),mod,div)::");
-			}
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        var action = "";
+        var goodAction = true;
+        System.out.println("Введите действие(/,+,-,*,^(второе чило целое),mod,div):");
+        do {
+            action = sc.next();
+            goodAction = action.equals("/") || action.equals("+") || action.equals("-") || action.equals("*")
+                    || action.equals("^") || action.equals("mod") || action.equals("div");
 
-		} while (!goodAction);
-		System.out.println("Введите первое число:");
-		number1 = sc.nextDouble();
+            if (!goodAction) {
+                System.out.println("Неверное действие.\nВведите действие(/,+,-,*,^(второе число целое),mod,div)::");
+            }
 
-		System.out.println("Введите второе число:");
-		number2 = sc.nextDouble();
-		do {
-			if (action.equals("/") && number2 == 0) {
-				System.out.println("На ноль делить нельзя\nВведите второе число:");
-				number2 = sc.nextDouble();
-			}
-		} while (number2 == 0.0);
+        }
+        while (!goodAction);
+        System.out.println("Введите первое число:");
+        var number1 = 0;
+        number1 = sc.nextInt();
+        System.out.println("Введите второе число:");
+        var number2 = 0;
+        number2 = sc.nextInt();
+        do {
+            if (action.equals("/") && number2 == 0) {
+                System.out.println("На ноль делить нельзя\nВведите второе число:");
+                number2 = sc.nextInt();
+            }
+            if (action.equals("^") && number2 < 0) {
+                System.out.println("Отрицательная степень!\nВведите второе число больше 0:");
+                number2 = sc.nextInt();
+            }
+        }
+        while ((action.equals("/") && number2 == 0) || (action.equals("^") && number2 < 0));
+        var result = 0;
+        switch (action) {
+            case "/":
+                result = number1 / number2;
+                break;
+            case "+":
+                result = number1 + number2;
+                break;
+            case "-":
+                result = number1 - number2;
+                break;
+            case "*":
+                result = number1 * number2;
+                break;
+            case "^":
+                result = myPow(number1, number2);
+                break;
+            case "mod":
+                result = number1 % number2;
+                break;
+            case "div":
+                result = number1 / number2;
+                break;
+            default:
+        }
 
-		if (action.equals("/")) {
-			result = number1 / number2;
-
-		} else if (action.equals("+")) {
-			result = number1 + number2;
-
-		} else if (action.equals("-")) {
-			result = number1 - number2;
-
-		} else if (action.equals("*")) {
-			result = number1 * number2;
-
-		} else if (action.equals("^")) {
-			result = myPow(number1, (long) number2);
-			System.out.println(result);
-
-		} else if (action.equals("mod")) {
-			result = number1 % number2;
-
-		} else if (action.equals("div")) {
-			result = (long) number1 / (long) number2;
-		}
-		System.out.println("Результат:");
-		System.out.println(result);
-
-	}
+        System.out.println("Результат:");
+        System.out.println(result);
+    }
 
 }
